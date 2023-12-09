e674cdd2868d5aad19ad38e09992b8f40f3b996e
PatrickSilvaMenezes <patricksilvamenezes@gmail.com>
2023-12-09 15:22:31 -0300

    feat: add control actions and tf without control actions

--- TFCSD/PID/algorithm.py
+++ TFCSD/PID/algorithm.py
@@ -16,24 +16,165 @@ class PID:
         Ploting the step response of the PID controller
         """
         # Calculando a função de transferência da malha fechada com o controlador PID
-        pid_controller = ctrl.TransferFunction([self.Kd, self.Kp, self.Ki], [1, 0])
+        pid_controller = ctrl.TransferFunction([self.Kd, self.Kp, self.Ki], [0, 1, 0])
 
-        print(pid_controller)
+        print("Gp: ", self.Tf)
+        print("Ação PID: ", pid_controller)
 
-        system_closed_loop = ctrl.feedback(self.Tf * pid_controller, self.feedback)
+        system_pid_closed_loop = ctrl.feedback(self.Tf * pid_controller, self.feedback)
 
         # Obtendo a resposta ao degrau da malha fechada
-        time, response = ctrl.step_response(system_closed_loop)
+        time_pid, response_pid = ctrl.step_response(system_pid_closed_loop)
 
         # Plotando a resposta ao degrau
-        plt.plot(time, response)
+        plt.plot(time_pid, response_pid)
         plt.xlabel('Tempo')
         plt.ylabel('Resposta ao Degrau')
         plt.title('Resposta ao Degrau da Malha Fechada com PID')
         plt.grid(True)
         plt.show()
 
+class PI:
+    def __init__(self, Kp: float, Ki: float, feedback: float, Tf: ctrl.tf):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Tf = Tf
+        self.feedback = feedback
 
+    def plot_step_response(self):
+        """"
+        Ploting the step response of the PI controller
+        """
+        # Calculando a função de transferência da malha fechada com o controlador PI
+        pi_controller = ctrl.TransferFunction([self.Kp, self.Ki], [0, 1])
+
+        print("Gp: ", self.Tf)
+        print("Ação PI: ", pi_controller)
+
+        system_pi_closed_loop = ctrl.feedback(self.Tf * pi_controller, self.feedback)
+
+        # Obtendo a resposta ao degrau da malha fechada
+        time_pi, response_pi = ctrl.step_response(system_pi_closed_loop)
+
+        # Plotando a resposta ao degrau
+        plt.plot(time_pi, response_pi)
+        plt.xlabel('Tempo')
+        plt.ylabel('Resposta ao Degrau')
+        plt.title('Resposta ao Degrau da Malha Fechada com PI')
+        plt.grid(True)
+        plt.show()
+
+class PI:
+    def __init__(self, Kp: float, Ki: float, feedback: float, Tf: ctrl.tf):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Tf = Tf
+        self.feedback = feedback
+
+    def plot_step_response(self):
+        """"
+        Ploting the step response of the PI controller
+        """
+        # Calculando a função de transferência da malha fechada com o controlador PI
+        pi_controller = ctrl.TransferFunction([self.Kp, self.Ki], [1, 0])
+
+        print("Gp: ", self.Tf)
+        print("Ação PI: ", pi_controller)
+
+        system_pi_closed_loop = ctrl.feedback(self.Tf * pi_controller, self.feedback)
+
+        # Obtendo a resposta ao degrau da malha fechada
+        time_pi, response_pi = ctrl.step_response(system_pi_closed_loop)
+
+        # Plotando a resposta ao degrau
+        plt.plot(time_pi, response_pi)
+        plt.xlabel('Tempo')
+        plt.ylabel('Resposta ao Degrau')
+        plt.title('Resposta ao Degrau da Malha Fechada com PI')
+        plt.grid(True)
+        plt.show()
+
+class PD:
+    def __init__(self, Kp: float, Kd: float, feedback: float, Tf: ctrl.tf):
+        self.Kp = Kp
+        self.Kd = Kd
+        self.Tf = Tf
+        self.feedback = feedback
+
+    def plot_step_response(self):
+        """"
+        Ploting the step response of the PI controller
+        """
+        # Calculando a função de transferência da malha fechada com o controlador PD
+        pd_controller = ctrl.TransferFunction([self.Kd, self.Kp], [0, 1])
+
+        print("Gp: ", self.Tf)
+        print("Ação PD: ", pd_controller)
+
+        system_pd_closed_loop = ctrl.feedback(self.Tf * pd_controller, self.feedback)
+
+        # Obtendo a resposta ao degrau da malha fechada
+        time_pd, response_pd = ctrl.step_response(system_pd_closed_loop)
+
+        # Plotando a resposta ao degrau
+        plt.plot(time_pd, response_pd)
+        plt.xlabel('Tempo')
+        plt.ylabel('Resposta ao Degrau')
+        plt.title('Resposta ao Degrau da Malha Fechada com PD')
+        plt.grid(True)
+        plt.show()
+
+class P:
+    def __init__(self, Kp: float, feedback: float, Tf: ctrl.tf):
+        self.Kp = Kp
+        self.Tf = Tf
+        self.feedback = feedback
+
+    def plot_step_response(self):
+        """"
+        Ploting the step response of the PI controller
+        """
+        # Calculando a função de transferência da malha fechada com o controlador P
+        p_controller = ctrl.TransferFunction([self.Kp], [1])
+
+        print("Gp: ", self.Tf)
+        print("Ação P: ", p_controller)
+
+        system_p_closed_loop = ctrl.feedback(self.Tf * p_controller, self.feedback)
+
+        # Obtendo a resposta ao degrau da malha fechada
+        time_p, response_p = ctrl.step_response(system_p_closed_loop)
+
+        # Plotando a resposta ao degrau
+        plt.plot(time_p, response_p)
+        plt.xlabel('Tempo')
+        plt.ylabel('Resposta ao Degrau')
+        plt.title('Resposta ao Degrau da Malha Fechada com P')
+        plt.grid(True)
+        plt.show()
+
+class GpNoControlAction:
+    def __init__(self, feedback: float, Tf: ctrl.tf):
+        self.Tf = Tf
+        self.feedback = feedback
+
+    def plot_step_response(self):
+        """"
+        Ploting the step response of the PI controller
+        """
+
+        print("Gp: ", self.Tf)
+
+        tf_closed_layer = ctrl.feedback(self.Tf, self.feedback)
+        time_gp, response_gp = ctrl.step_response(tf_closed_layer)
+
+        # Criando um vetor de tempo mais adequado
+        plt.plot(time_gp, response_gp)
+        plt.xlabel('Tempo')
+        plt.ylabel('Resposta ao Degrau')
+        plt.title('Resposta ao Degrau da Gp em Malha Fechada sem Ação de Controle')
+        plt.grid(True)
+        plt.show()
 
 
 
@@ -57,6 +198,35 @@ def main():
         Tf = Tf
     )
 
+    pi = PI(
+        Kp = dados_entrada['Kp'],
+        Ki = dados_entrada['Ki'],
+        feedback = dados_entrada['feedback'],
+        Tf = Tf
+    )
+
+    pd = PD(
+        Kp = dados_entrada['Kp'],
+        Kd = dados_entrada['Kd'],
+        feedback = dados_entrada['feedback'],
+        Tf = Tf
+    )
+
+    p = P(
+        Kp = dados_entrada['Kp'],
+        feedback = dados_entrada['feedback'],
+        Tf = Tf
+    )
+
+    tfmf = GpNoControlAction(
+        feedback = dados_entrada['feedback'],
+        Tf = Tf
+    )
+
+    tfmf.plot_step_response()
+    p.plot_step_response()
+    pi.plot_step_response()
+    pd.plot_step_response()
     pid.plot_step_response()
 
 if __name__ == '__main__':
