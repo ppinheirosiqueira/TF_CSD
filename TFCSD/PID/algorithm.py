17699eacdf5e08b2ed320733128776ec7056393e
ppinheirosiqueira <pedropinheirodesiqueira94@gmail.com>
2023-12-11 19:53:33 -0300

    Retirando prints e main inutil

--- TFCSD/PID/algorithm.py
+++ TFCSD/PID/algorithm.py
@@ -19,9 +19,6 @@ class PID:
         # Calculando a função de transferência da malha fechada com o controlador PID
         pid_controller = ctrl.TransferFunction([self.Kp, self.Ki, self.Kd], [0, 1, 0])
 
-        print("Gp: ", self.Tf)
-        print("Ação PID: ", pid_controller)
-
         system_pid_closed_loop = ctrl.feedback(self.Tf * pid_controller, self.feedback)
 
         # Obtendo a resposta ao degrau da malha fechada
@@ -52,9 +49,6 @@ class PI:
         # Calculando a função de transferência da malha fechada com o controlador PI
         pi_controller = ctrl.TransferFunction([self.Kp, self.Ki], [1, 0])
 
-        print("Gp: ", self.Tf)
-        print("Ação PI: ", pi_controller)
-
         system_pi_closed_loop = ctrl.feedback(self.Tf * pi_controller, self.feedback)
 
         # Obtendo a resposta ao degrau da malha fechada
@@ -84,9 +78,6 @@ class PD:
         # Calculando a função de transferência da malha fechada com o controlador PD
         pd_controller = ctrl.TransferFunction([self.Kd, self.Kp], [0, 1])
 
-        print("Gp: ", self.Tf)
-        print("Ação PD: ", pd_controller)
-
         system_pd_closed_loop = ctrl.feedback(self.Tf * pd_controller, self.feedback)
 
         # Obtendo a resposta ao degrau da malha fechada
@@ -115,9 +106,6 @@ class P:
         # Calculando a função de transferência da malha fechada com o controlador P
         p_controller = ctrl.TransferFunction([self.Kp], [1])
 
-        print("Gp: ", self.Tf)
-        print("Ação P: ", p_controller)
-
         system_p_closed_loop = ctrl.feedback(self.Tf * p_controller, self.feedback)
 
         # Obtendo a resposta ao degrau da malha fechada
@@ -143,8 +131,6 @@ class GpNoControlAction:
         Ploting the step response of the PI controller
         """
 
-        print("Gp: ", self.Tf)
-
         tf_closed_layer = ctrl.feedback(self.Tf, self.feedback)
         time_gp, response_gp = ctrl.step_response(tf_closed_layer)
 
@@ -160,57 +146,3 @@ class GpNoControlAction:
 
 def fazerTF(numerador,denominador):
     return ctrl.tf(numerador, denominador)
-
-# def main():
-#     dados_entrada = {
-#         'Kp': 1,
-#         'Ki': 1,
-#         'Kd': 1,
-#         'numerador': [1],
-#         'denominador': [1, 2, 1],
-#         'feedback': 1
-#     }
-
-#     Tf = ctrl.tf(dados_entrada['numerador'], dados_entrada['denominador'])
-
-#     pid = PID(
-#         Kp = dados_entrada['Kp'],
-#         Ki = dados_entrada['Ki'],
-#         Kd = dados_entrada['Kd'],
-#         feedback = dados_entrada['feedback'],
-#         Tf = Tf
-#     )
-
-#     pi = PI(
-#         Kp = dados_entrada['Kp'],
-#         Ki = dados_entrada['Ki'],
-#         feedback = dados_entrada['feedback'],
-#         Tf = Tf
-#     )
-
-#     pd = PD(
-#         Kp = dados_entrada['Kp'],
-#         Kd = dados_entrada['Kd'],
-#         feedback = dados_entrada['feedback'],
-#         Tf = Tf
-#     )
-
-#     p = P(
-#         Kp = dados_entrada['Kp'],
-#         feedback = dados_entrada['feedback'],
-#         Tf = Tf
-#     )
-
-#     tfmf = GpNoControlAction(
-#         feedback = dados_entrada['feedback'],
-#         Tf = Tf
-#     )
-
-#     tfmf.plot_step_response()
-#     p.plot_step_response()
-#     pi.plot_step_response()
-#     pd.plot_step_response()
-#     pid.plot_step_response()
-
-# if __name__ == '__main__':
-#     main()
